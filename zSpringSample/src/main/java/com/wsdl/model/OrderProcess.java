7fb4b149d6fe97ed4ec3dd93200fe2739e1c72e4
guitarhyo <guitarhyo@gmail.com>
2018-06-28 13:26:12 +0900

    태그 이름 추가

--- zSpringSample/src/main/java/com/wsdl/model/OrderProcess.java
+++ zSpringSample/src/main/java/com/wsdl/model/OrderProcess.java
@@ -1,8 +1,9 @@
 package com.wsdl.model;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface  OrderProcess {
-	String processOrder(String user);
+	String processOrder(@WebParam(name = "tcms")String user);
 }
