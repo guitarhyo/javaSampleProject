0d0e6460cbc3db5d49cc580517f9e948c164941d
guitarhyo <guitarhyo@gmail.com>
2018-08-09 17:18:53 +0900

    JSON컨트롤러 추가 jackson 라이브러리

--- zSpringSample/src/main/java/com/sp/CommonController.java
+++ zSpringSample/src/main/java/com/sp/CommonController.java
@@ -1,25 +1,16 @@
 package com.sp;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.wsdl.extensions.soap.SOAPBody;
-import javax.xml.namespace.QName;
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPBodyElement;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Service;
-import javax.xml.ws.WebServiceException;
-import javax.xml.ws.soap.SOAPBinding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Controller
@@ -32,13 +23,25 @@ public class CommonController {
 	HttpServletResponse response;
 	
 	@RequestMapping(value = "/test/json")
-	public @ResponseBody ObjectNode jsonreturn() throws Exception {
-		 ObjectMapper objectMapper = new ObjectMapper();
-		 ObjectNode node = objectMapper.createObjectNode();
-		
-	
-		
-		
+	public @ResponseBody ObjectNode jsonreturn(@RequestBody ObjectNode node) throws Exception {
+//		 ObjectMapper objectMapper = new ObjectMapper();
+//		 ObjectNode node = objectMapper.createObjectNode();
+		 
+		 System.out.println(node.toString());
+		System.out.println("--------------------");
+		 String queryString = request.getQueryString();
+		 System.out.println(queryString);
+		 
+		 Enumeration params = request.getParameterNames();
+		 System.out.println("----------------------------");
+		 while (params.hasMoreElements()){
+		     String name = (String)params.nextElement();
+		     System.out.println(name + " : " +request.getParameter(name));
+		 }
+
+
+		 node.put("test", "1234");
+		 node.put("test2", "테스트");
 		return node;
 	}
 
